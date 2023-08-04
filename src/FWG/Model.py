6879cadb6ce9090d0df1d02534f4f566186a6e2f
Yuanhao Li <erwinli@qq.com>
2023-08-04 11:23:36 +0100

    fix import; fix error string

--- src/FWG/Model.py
+++ src/FWG/Model.py
@@ -1,5 +1,5 @@
 import os
-from gensim import downloader as downloader
+from gensim import downloader
 class Word2vec_model:
     def __init__(self, model_name=None):
         self.model_name = model_name
@@ -25,7 +25,7 @@ class Word2vec_model:
         return self
     def load(self):
         if self.model_name == None:
-            raise Exception("Please set model tobe loaded, 'model.set_model(\"glove-twitter-25\")")
+            raise Exception("Please set model to be loaded, 'model.set_model(\"glove-twitter-25\")")
         self.model=  downloader.load(self.model_name)
         return self
     def vectorize(self, W):
